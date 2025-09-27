fc3e4e0021ce19c4a7c627f2e29db9608c0a9786
einosukeokazaki <52006822+PenguinEino@users.noreply.github.com.>
2025-09-28 02:02:10 +0900

    change ci script

--- flutter/tmcit_beacon/ios/ci_scripts/ci_post_clone.sh
+++ flutter/tmcit_beacon/ios/ci_scripts/ci_post_clone.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Fail this script if any subcommand fails.
-set -e
+set -ex
 
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
