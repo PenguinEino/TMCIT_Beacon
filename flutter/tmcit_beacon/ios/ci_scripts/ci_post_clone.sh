73cc4e5b51e2369ed3acc422a9c535d3b80d551f
einosukeokazaki <52006822+PenguinEino@users.noreply.github.com.>
2025-09-28 02:05:53 +0900

    change ci script

--- flutter/tmcit_beacon/ios/ci_scripts/ci_post_clone.sh
+++ flutter/tmcit_beacon/ios/ci_scripts/ci_post_clone.sh
@@ -5,6 +5,8 @@ set -ex
 
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
+echo "Working directory: $(pwd)"
+ls -l  # 確認用（pubspec.yaml があるか）
 
 # Install Flutter using git.
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
